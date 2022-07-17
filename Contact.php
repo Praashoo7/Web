9467697dc8d1bc518e041fe1b6553ea15cccc246
Prashant Patil <rnttl90@gmail.com>
2022-07-17 22:14:13 +0530

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "ssl://smtp.gmail.com";
+$username = "fromtome99@gmail.com";
+$password = "fsfsfsfs";
+$port = "465";
+$to = "prashant09855@gmail.com";
+$email_from = $_POST['email'];
+$email_subject = "Mail From me";
+$email_body = $_POST['message'];
+$email_address = "reply-to@example.com";
+
+$headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
+$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
+$mail = $smtp->send($to, $headers, $email_body);
+
+
+if (PEAR::isError($mail)) {
+echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+echo("<p>Message successfully sent!</p>");
+}
+?>
